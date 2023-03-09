c0f4092f822da598bd6770739a61f22f7fccf44a
Mehmet Nadi <mhmtalperennadi@gmail.com>
2023-03-09 11:09:33 -0800

    display msg sent time

--- src/componenets/Messages.jsx
+++ src/componenets/Messages.jsx
@@ -6,7 +6,22 @@ export default function Messages({ message }) {
   const { user } = UserAuth();
 
   const ref = useRef();
-  console.log(message);
+
+  const convertSecondToDate = (second) => {
+    const date = new Date(second * 1000);
+    // Hours part from the timestamp
+    const hours = date.getHours();
+    // Minutes part from the timestamp
+    const minutes = "0" + date.getMinutes();
+    // Seconds part from the timestamp
+    const seconds = "0" + date.getSeconds();
+
+    // Will display time in 10:30:23 format
+    const formattedTime =
+      hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
+
+    return formattedTime;
+  };
 
   useEffect(() => {
     ref.current?.scrollIntoView({ behavior: "smooth" });
@@ -20,7 +35,9 @@ export default function Messages({ message }) {
     >
       <span className="chat_name">{message.senderName}</span>
       {message.newMsg}
-      <span className="chat_timestamp">Now</span>
+      <span className="chat_timestamp">
+        {convertSecondToDate(message.date?.seconds)}
+      </span>
     </p>
   );
 }
