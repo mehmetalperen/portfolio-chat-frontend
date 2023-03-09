49aa94768981ad1c9fa3cea807752c336d91ad03
Mehmet Nadi <mhmtalperennadi@gmail.com>
2023-03-09 11:10:06 -0800

    remove comment

--- src/componenets/Messages.jsx
+++ src/componenets/Messages.jsx
@@ -9,14 +9,10 @@ export default function Messages({ message }) {
 
   const convertSecondToDate = (second) => {
     const date = new Date(second * 1000);
-    // Hours part from the timestamp
     const hours = date.getHours();
-    // Minutes part from the timestamp
     const minutes = "0" + date.getMinutes();
-    // Seconds part from the timestamp
-    const seconds = "0" + date.getSeconds();
 
-    // Will display time in 10:30:23 format
+    const seconds = "0" + date.getSeconds();
     const formattedTime =
       hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
 
